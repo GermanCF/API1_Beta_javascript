f9c38416b3edc38810796f4de5da1c08d71820d0
German Carro Fernandez <gmncrf@gmail.com>
2014-05-22 14:51:55 +0200

    Arduino code

---
+++ test/led/Arduino_code/led_code.ino
@@ -0,0 +1,18 @@
+int led =11;
+void setup(){
+pinMode(led, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop(){
+if (Serial.available()){
+char c = Serial.read();
+if (c == 'H') {
+digitalWrite(led, HIGH);
+}
+else if (c == 'L') {
+digitalWrite(led, LOW);
+}
+}
+
+}
